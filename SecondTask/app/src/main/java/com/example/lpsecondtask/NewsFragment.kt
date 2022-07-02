2b416a49014e8c85957d72871c8ba31c50b1cc8a
ValerySlobodchikova <merzlova.lubov@gmail.com>
2022-07-02 13:25:32 +0300

    Second task

---
+++ SecondTask/app/src/main/java/com/example/lpsecondtask/NewsFragment.kt
@@ -0,0 +1,26 @@
+package com.example.lpsecondtask
+
+import android.content.Context
+import android.os.Bundle
+import android.provider.Settings.Global.putString
+import android.view.View
+import androidx.core.content.edit
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+
+class NewsFragment : Fragment(R.layout.fragment_news) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val pref = activity?.getSharedPreferences("TEST", Context.MODE_PRIVATE) ?: return
+        val value = pref.getString("PREF_TEXT", "").orEmpty()
+        val text = this.javaClass.name
+        pref.edit {
+            putString("PREF_TEXT", text)
+            commit()
+        }
+
+        if (!value.isEmpty())
+            Snackbar.make(view, value, Snackbar.LENGTH_SHORT).show()
+    }
+}
